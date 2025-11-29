d916687c9c1ab7cfcf9f2c6e9dd45fda91a10813
Varga Peter <varga.peter2500@gmail.com>
2025-11-29 16:24:57 +0100

    Measure multiple errors

--- measure.py
+++ measure.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.optimize import brentq
 from dataclasses import dataclass
 from time import perf_counter
 import tabulate
@@ -15,7 +16,25 @@ class TestResult:
     y: np.ndarray
     error: float = 0
 
-def measure_method(method, name, y0, Tl, Tr, N, f_exact) -> TestResult:
+def measure_method(method, name, y0, Tl, Tr, f_exact, target_error, N_ = 2000, N_min = 2000, N_max = 10000) -> TestResult:
+    def f_impl(n):
+        n = min(max(int(n), 1), N_max)
+        h, t, y = method(y0, Tl, Tr, n)
+        return target_error - np.linalg.norm(y - f_exact(t), np.inf)
+    
+    if abs(f_impl(N_)) < target_error / 10:
+        N = N_
+    else:
+        l_val = f_impl(N_min)
+        r_val = f_impl(N_max)
+
+        if l_val > 0:
+            N = N_min
+        elif r_val < 0:
+            N = N_max
+        else:
+            N = int(brentq(f_impl, N_min, N_max, xtol=1))
+
     start = perf_counter()
     h, t, y = method(y0, Tl, Tr, N)
     end = perf_counter()
@@ -32,10 +51,8 @@ def measure_method(method, name, y0, Tl, Tr, N, f_exact) -> TestResult:
         error
     )
 
-def get_table(results) -> str:
+def get_table_data(results) -> str:
     data = []
     for result in results:
         data.append([result.name, f'{result.exec_time:.5f}', f'{result.h:.5f}', f'{result.error:.5f}'])
-
-    table = tabulate.tabulate(data, headers=['Name', 'Exec time (s)', 'Step Size', 'Error'], tablefmt='html')
-    return table
+    return data
